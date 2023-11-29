c478c630b6375723493d749accb4340a31b105c0
Arshim <98533618+Arshim28@users.noreply.github.com>
2023-11-29 15:15:11 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,3 +16,17 @@ def load_model_config(model_config_path):
     with open(model_config_path, "r") as config_file:
         config = json.load(config_file)
     return config
+
+def evaluate(predictions, ground_truth):
+    correct_count = 0
+    total_count = len(ground_truth)
+
+    for prediction, truth in zip(predictions, ground_truth):
+        predicted_answer = prediction["answer"]
+        true_answer = truth["answer"]
+
+        if predicted_answer == true_answer:
+            correct_count += 1
+
+    accuracy = correct_count / total_count
+    return accuracy
